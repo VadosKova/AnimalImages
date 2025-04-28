02f879b58d32791ca0d04caf3a937a73f2760965
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-04-28 14:33:00 +0300

    Created main class and added connection with db

--- server.py
+++ server.py
@@ -3,3 +3,14 @@ import jsonpickle
 import pyodbc
 import requests
 import logging
+
+
+class User:
+    def __init__(self, username=None, email=None, password=None):
+        self.username = username
+        self.email = email
+        self.password = password
+
+        self.connection_string = ('DRIVER={ODBC Driver 17 for SQL Server};SERVER=localhost;DATABASE=AnimalImages;Trusted_Connection=yes')
+        self.conn = pyodbc.connect(self.connection_string)
+        self.cursor = self.conn.cursor()
