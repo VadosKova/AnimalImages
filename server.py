9ccc3dcc05fc45f415482f421a5f8ac403c4209b
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-04-28 14:35:02 +0300

    Created function for checking on admin

--- server.py
+++ server.py
@@ -14,3 +14,9 @@ class User:
         self.connection_string = ('DRIVER={ODBC Driver 17 for SQL Server};SERVER=localhost;DATABASE=AnimalImages;Trusted_Connection=yes')
         self.conn = pyodbc.connect(self.connection_string)
         self.cursor = self.conn.cursor()
+
+
+    def is_admin(self):
+        self.cursor.execute('SELECT IsAdmin FROM Users WHERE LOWER(Username) = LOWER(?)', (self.username,))
+        result = self.cursor.fetchone()
+        return result and result[0] == 1
