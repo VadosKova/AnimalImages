1774a1a28141e151a951a072a9ae980464fc7733
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-04-29 18:37:50 +0300

    Created branch and file for client

---
+++ client.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter import messagebox, filedialog
+import socket
+import jsonpickle
+import requests
+import os
+from PIL import Image, ImageTk
+
+IP = '127.0.0.1'
+PORT = 4000
