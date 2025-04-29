f0c96de384289af282d083fa67b4239ebab49ef5
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-04-29 18:49:45 +0300

    Created window for authorization

--- client.py
+++ client.py
@@ -33,3 +33,18 @@ class AnimalImageApp:
         for widget in self.root.winfo_children():
             widget.destroy()
         self.image_references = []
+
+    def start_screen(self):
+        self.clear_widgets()
+        Label(self.root, text="Animal Images", font=self.header_font, bg='#f0f0f0').pack(pady=20)
+
+        Label(self.root, text="Username:").pack()
+        self.username_entry = Entry(self.root)
+        self.username_entry.pack()
+
+        Label(self.root, text="Password:").pack()
+        self.password_entry = Entry(self.root, show="*")
+        self.password_entry.pack()
+
+        Button(self.root, text="Login", command=self.login).pack(pady=10)
+        Button(self.root, text="Register", command=self.register_screen).pack()
