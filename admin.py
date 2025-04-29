ff46a34a7b3e7a2f8c07fc54adb3ecdb1a75417d
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-04-29 23:10:22 +0300

    Created file for admin

---
+++ admin.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter import messagebox, ttk
+import socket
+import jsonpickle
+import requests
+import os
+from PIL import Image, ImageTk
+
+IP = '127.0.0.1'
+PORT = 4000
